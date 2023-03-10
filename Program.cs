4024bf7f0a852256457a7b250485072f525f4b99
Sasha <sashok.ey@mail.ru>
2023-03-10 11:46:18 +0800

    method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿void Proverka(string[] array, string[] array2)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[i] = array[i];
+        }
+    }
+}
